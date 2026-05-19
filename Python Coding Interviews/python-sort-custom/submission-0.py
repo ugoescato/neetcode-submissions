829323943c0c4d617ed40fa281540d95f6bf4d2d
Ugo Escato <ugo.escato@edhec.com>
2026-05-19 23:51:01 +0200

    Add: python-sort-custom - submission-0

---
+++ Python Coding Interviews/python-sort-custom/submission-0.py
@@ -0,0 +1,18 @@
+from typing import List
+
+def get_word_len(word: str):
+    return len(word)
+
+def get_num_abs(numb: int):
+    return abs(numb)
+
+def sort_words(words: List[str]) -> List[str]:
+    return sorted(words, key=get_word_len, reverse=True)
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    return sorted(numbers, key=get_num_abs)
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]))
