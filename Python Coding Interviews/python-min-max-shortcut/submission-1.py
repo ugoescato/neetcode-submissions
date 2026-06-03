15092a672a5f13e252cdbdbff6a043c5b6f45538
Ugo Escato <ugo.escato@edhec.com>
2026-06-04 00:34:57 +0200

    Add: python-min-max-shortcut - submission-1

---
+++ Python Coding Interviews/python-min-max-shortcut/submission-1.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+def disallow_negatives(num: int) -> int:
+    return num if num >= 0 else 0
+
+
+def max_difference(nums: List[int]) -> int:
+    maxi = 0
+
+    for i in range(1, len(nums)):
+        if nums[i] - nums[i-1] > maxi:
+            maxi = nums[i] - nums[i-1]
+
+    return maxi
+
+# do not modify below this line
+print(disallow_negatives(-2))
+print(disallow_negatives(-1))
+print(disallow_negatives(0))
+print(disallow_negatives(1))
+print(disallow_negatives(2))
+
+print(max_difference([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(max_difference([1, 2, 3, 4, 5, 6, 8, 9]))
+print(max_difference([10, 1, 3, 7]))
+print(max_difference([2, 4, 7, 5, 7, 8, 4, 2]))
